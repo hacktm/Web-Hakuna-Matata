4ec4b7c043d4c6a8c87c7947e246f8331b204453
Mircea Tihu <mirceatihu@gmail.com>
2014-10-18 11:17:56 +0300

    Added header markup

--- header.php
+++ header.php
@@ -6,4 +6,12 @@
 	<?php wp_head(); ?>
 </head>
 <body>
-
+	<header>
+		<a id="logo" href="<?php echo home_url();  ?>"><?php echo get_bloginfo('name');  ?></a>
+		<nav id="main-menu">
+			<a href="<?php echo wp_login_url(); ?>"><?php _e( 'Log in' ,'tips4trip' ); ?></a>
+			<a href="<?php echo wp_registration_url(); ?>"><?php _e( 'Register', 'tips4trip' ); ?></a>
+			<a href="<?php echo get_admin_url(); ?>"><?php _e( 'Dashboard' ,'tips4trip' ); ?></a>
+			<a href="<?php echo wp_logout_url( home_url() ); ?>"><?php _e( 'Log out' ,'tips4trip' ); ?></a>
+		</nav>
+	</header>
