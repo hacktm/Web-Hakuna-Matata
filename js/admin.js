4ecc200f34511359f43ba4a39e106bbd4d85298a
Daniel Telbis <daniel_telbis@yahoo.com>
2014-10-18 15:13:32 +0300

    Added google map generator Added set location on post

--- js/admin.js
+++ js/admin.js
@@ -2,5 +2,24 @@
 	$(document).ready(function() {
 		var countries = $('#travel-map').data('countries');
 		console.log(countries);
+
+		var location = $('#location-selection-map');
+		if( location.length ) {
+			var lat = $('#lat');
+			var lng = $('#lng');
+			var latitude = lat.val() ? lat.val() : 0;
+			var longitude = lng.val() ? lng.val() : 0;
+
+
+			var dragend = function() {
+				var new_position = this.getPosition();
+				lat.val( new_position.lat() );
+				lng.val( new_position.lng() );
+			};
+
+			var map = new MapGenerator();
+			map.generateMap( 'location-selection-map', latitude, longitude, 3 );
+			map.setCenterMarker({ title: "", info: "", clickable: true, draggable: true, dragendHandler: dragend });
+		}
 	});
 })(jQuery);
