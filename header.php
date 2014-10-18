52c4d14a69b4e173d39c50eecfb0d0578b2d5721
Mircea Tihu <mirceatihu@gmail.com>
2014-10-18 12:42:44 +0300

    Styled header

--- header.php
+++ header.php
@@ -5,13 +5,16 @@
 	<title><?php bloginfo( 'title' ); ?></title>
 	<?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 	<header>
 		<a id="logo" href="<?php echo home_url();  ?>"><?php echo get_bloginfo('name');  ?></a>
 		<nav id="main-menu">
-			<a href="<?php echo wp_login_url(); ?>"><?php _e( 'Log in' ,'tips4trip' ); ?></a>
-			<a href="<?php echo wp_registration_url(); ?>"><?php _e( 'Register', 'tips4trip' ); ?></a>
-			<a href="<?php echo get_admin_url(); ?>"><?php _e( 'Dashboard' ,'tips4trip' ); ?></a>
-			<a href="<?php echo wp_logout_url( home_url() ); ?>"><?php _e( 'Log out' ,'tips4trip' ); ?></a>
+			<?php if( ! is_user_logged_in() ) { ?>
+				<a href="<?php echo wp_login_url(); ?>"><?php _e( 'Log in' ,'tips4trip' ); ?></a>
+				<a href="<?php echo wp_registration_url(); ?>"><?php _e( 'Register', 'tips4trip' ); ?></a>
+			<?php } else { ?>
+				<a href="<?php echo get_admin_url(); ?>"><?php _e( 'Dashboard' ,'tips4trip' ); ?></a>
+				<a href="<?php echo wp_logout_url( home_url() ); ?>"><?php _e( 'Log out' ,'tips4trip' ); ?></a>
+			<?php } ?>
 		</nav>
 	</header>
