2383b5d9593de827d598361e3804166953d9f355
Mircea Tihu <mirceatihu@gmail.com>
2014-10-19 13:43:21 +0300

    Author page is now responsive

--- js/general.js
+++ js/general.js
@@ -1,48 +1,3 @@
-// This is called with the results from from FB.getLoginStatus().
-function statusChangeCallback(response) {
-	if (response.status === 'connected') {
-		// Logged into your app and Facebook.
-		FB.api('/me', function(response) {
-			console.log(response);
-		});
-
-	}
-}
-
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-	FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-	});
-}
-
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : 1585578998249136,
-		cookie     : true,  // enable cookies to allow the server to access the session
-		xfbml      : true,  // parse social plugins on this page
-		version    : 'v2.1' // use version 2.1
-	});
-
-
-	FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-	});
-
-};
-
-// Load the SDK asynchronously
-(function(d, s, id) {
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) return;
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/en_US/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
 (function ($) {	
 	function drawMap() {
 		var data = google.visualization.arrayToDataTable($('#main-geo-map').data('count'));
@@ -80,11 +35,7 @@ window.fbAsyncInit = function() {
 			var window_height = $(window).outerHeight();
 			var header_height = $('header').outerHeight();
 			var admin_bar_height = $('#wpadminbar').outerHeight();
-			if( $(this).attr('id') == 'user-info' )
-				$(this).css( 'min-height', window_height - header_height - admin_bar_height - 40 + 'px');
-			else
-				$(this).css( 'min-height',  window_height - header_height - admin_bar_height + 'px' );
-
+			$(this).css( 'min-height', window_height - header_height - admin_bar_height  + 'px');
 		});
 
 		if( $('#main-geo-map').length )
